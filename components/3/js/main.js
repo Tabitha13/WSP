1df6a417b6840d4e13c2d460438bc46f16217ba8
Tabitha13 <tnwestfall@fullsail.edu>
2015-10-23 13:03:38 -0400

    fix

---
+++ components/3/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by tabithawestfall on 10/17/15.
+ */
+
+$(document).ready(function(){
+
+    $(".top-menu").slideupmenu({
+
+            slideUpSpeed: 150,
+
+    slideDownSpeed: 200,
+
+    ease: "easeOutQuad",
+
+    stopQueue: true
+
+});
+
+});
